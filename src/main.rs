3bb511690e400e665975177755eccc21cb83dd04
Andrey Moor <m@andreym.com>
2025-07-29 09:54:28 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 /// A "Hello, world!" program
-/// Googbye
 
 fn main() {
     print_hello
+    print_goodbye
 }
 
 fn print_hello () {
     println!("Hello, world!");
+}
+
+fn print_goodbye () {
+    println!("Goodbye, world!");
 }
